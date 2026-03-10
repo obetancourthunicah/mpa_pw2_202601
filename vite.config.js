8cda13604dfc4ea46b9de8ce3226ef3841db9de7
Orlando Betancourth <obetancourthunicah@gmail.com>
2026-03-09 19:55:57 -0600

    chore: added base variable to deploy

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,7 @@ function obtenerHtmlFiles(){
 export default defineConfig(
     {
         appType: 'mpa',
+        base: process.env.DEPLOY_BASE_URL ?? '/' ,
         build: {
             rollupOptions: {
                 input: obtenerHtmlFiles(),
